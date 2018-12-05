0e54da8fb1a4141b80a6a67e9c56d029e8bcd5b5
chenx16 <chenx16@rose-hulman.edu>
2018-12-05 14:52:29 -0500

    05b

--- src/problem0.py
+++ src/problem0.py
@@ -9,8 +9,8 @@ These problems illustrate concepts that previous problems have not emphasized:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Valerie Galluzzi, Mark Hays, Amanda Stouder, Aaron Wilkin,
-         their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues, and Xinlai Chen.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 import testing_helper
@@ -25,7 +25,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the green doc-string for the:
+# done: 2.  READ the green doc-string for the:
 #   - is_prime
 #   - sum_of_digits
 # functions defined below.  You do NOT need to understand their
@@ -187,9 +187,18 @@ def problem0a(n):
            since (3 + 0 + 6) is 9, which IS odd.
       -- If the given integer is 246, this function returns False,
            since (2 + 4 + 6) is 12, which is NOT odd.
-    """
+    # """
+    # sod=sum_of_digits(n)
+    # if sod %2 ==1:
+    #     return True
+    # else:
+    #     return False
+    #Alternative:
+
+    return (sum_of_digits(n)%2==1)
+
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # done: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -261,8 +270,20 @@ def problem0b(n):
       -- If n is 200, the correct answer is 46,
            since there are 46 primes between 2 and 200.
      """
+    count=0
+    for k in range(n-1):
+        if is_prime(k+2) is True:
+            count=count+1
+    return count
+
+    # #Alternative:
+    # count=0
+    # for k in range (n-1):
+    #     if is_prime(k + 2):
+    #         count = count + 1
+    # return count
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # done: 4. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -329,8 +350,19 @@ def problem0c(circle, n, window):
       :type n: int
       :type window: rg.RoseWindow
     """
+    circle.attach_to(window)
+    diameter=2*circle.radius
+    x=circle.center.x+diameter
+    y=circle.center.y
+
+    for _ in range(n):
+        c=rg.Circle(rg.Point(x,y),circle.radius)
+        c.attach_to(window)
+        window.render(0.5)
+
+        x=x+diameter
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # done: 5. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
